0a922687ab5a29349bce20adf593ed370b9b45e4
martin <holgado.m@gmail.com>
2017-06-11 20:41:45 -0300

    se movió el config de $auth al app.js para que sea funcional a todos los controladores

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,13 @@
 var app = angular.module("LaMaceta", ["ui.bootstrap", "ngAnimate", "ngSanitize", "ngTable", "checklist-model", "ngCookies", "ngFileUpload", "satellizer"]);
+
+app.config(function($authProvider){
+
+
+		// $authProvider.loginUrl='LaMaceta/PHP/clases/autentificador.php';
+		$authProvider.loginUrl='LaMaceta/bd/LoginBd.php';
+		// $authProvider.signupUrl='LaMaceta/PHP/clases/autentificador.php';
+		$authProvider.tokenName='tokenMaceta'
+		$authProvider.tokenPrefix= 'maceta';
+		$authProvider.authHeader= 'Data';
+
+	}); // fin config
--- js/controller/LoginController.js
+++ js/controller/LoginController.js
@@ -1,16 +1,3 @@
-// angular.module("LaMaceta")
-app.config(function($authProvider){
-
-
-	// $authProvider.loginUrl='LaMaceta/PHP/clases/autentificador.php';
-	$authProvider.loginUrl='LaMaceta/bd/LoginBd.php';
-	// $authProvider.signupUrl='LaMaceta/PHP/clases/autentificador.php';
-	$authProvider.tokenName='tokenMaceta'
-	$authProvider.tokenPrefix= 'maceta';
-	$authProvider.authHeader= 'Data';
-
-}); // fin config
-
 angular.module("LaMaceta")
 .directive("passwordVerify", function() {
    return {
